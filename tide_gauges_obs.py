4ae90a0691125b847c061b02c88dabddb2cc260d
ROWAN BROWN <rjb641@mun.ca>
2024-01-31 09:42:20 -0700

    Script for plotting tide gauges

--- tide_gauges_obs.py
+++ tide_gauges_obs.py
@@ -1,7 +1,7 @@
 # initial looking at observations, tide gauges specifically
 
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, date
 import matplotlib.pyplot as plt
 
 file_name = {
@@ -10,31 +10,37 @@ file_name = {
         'LoweInlet': 'LoweInlet_2014_forConstituents.csv'
     }
 
-fig_title = {
-        'PrinceRupert': 'Tide gauge in Prince Rupert',
-        'HartleyBay': 'Tide gauge in Hartley Bay',
-        'LoweInlet': 'Tide gauge in Lowe Inlet'
-    }
-
 c1, c2, c3 = plt.cm.viridis([0, 0.5, 0.8])
 
-def tide_gauges(location):
+def tide_gauges():
 
     tidal_gauges_obs_dir_path = '/mnt/storage3/tahya/DFO/Observations/Tide_Gauges/'
-    obs_path = tidal_gauges_obs_dir_path + file_name[location]
 
-    df = pd.read_csv(obs_path, sep=",", header=None,engine='python')
-    df = df.drop(index=range(0,8), columns=[2])
-    df.columns = ['original_date','val']
-    df['datetime_date'] = pd.to_datetime(df['original_date'],format = "%Y/%m/%d %H:%M")
-    df['val'] = df['val'].astype(float)
+    df_list = []
+    for location in file_name.keys():
+        obs_path = tidal_gauges_obs_dir_path + file_name[location]
+        df = pd.read_csv(obs_path, sep=",", header=None,engine='python')
+        df = df.drop(index=range(0,8), columns=[2])
+        df.columns = [location+'_date',location+'_val']
+        df[location+'_date'] = pd.to_datetime(df[location+'_date'],format = "%Y/%m/%d %H:%M")
+        df[location+'_val'] = df[location+'_val'].astype(float)
+        df_list.append(df)
 
+    df = pd.concat(df_list,axis=1)
+    fig,ax1 = plt.subplots()
+    df.plot(x='PrinceRupert_date', y='PrinceRupert_val', ax=ax1, color=c1)
+    df.plot(x='HartleyBay_date', y='HartleyBay_val', ax=ax1, color=c2)
+    df.plot(x='LoweInlet_date', y='LoweInlet_val', ax=ax1, color=c3)
 
-    plt.figure()
-    df.plot(x='datetime_date', y='val', title=fig_title[location], legend=None, color=c)
+    ax1.set_title('Tide Gauges')
+    ax1.set_ylabel('SSH')
+    ax1.set_xlabel('Date')
+    ax1.set_xlim([date(2020, 1, 1), date(2020, 1, 7)])
 
-    plt.savefig('plots/' 'test.png',dpi=300, bbox_inches="tight")
-    plt.clf()
+    fig.savefig('plots/tide_gauges.png',dpi=300, bbox_inches="tight")
+    fig.clf()
+
+    print('done')
 
 if __name__ == "__main__":
-    tide_gauges('PrinceRupert')
+    tide_gauges()
