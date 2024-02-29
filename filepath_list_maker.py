c29a063ae6a8ec6c4d28839362f4095e3cdcd61e
ROWAN BROWN <rjb641@mun.ca>
2024-02-29 14:11:07 -0700

    Added functionality for kit500 nc files

--- filepath_list_maker.py
+++ filepath_list_maker.py
@@ -7,15 +7,17 @@
 import os
 import csv 
 
-def filepaths(): 
+def filepaths(model): 
 
 	#directories of nemo output "sub"directories
 	grc_storage3 = '/mnt/storage3/tahya/DFO/grc100_model_results/'
 	grc_storage5 = '/mnt/storage5/tahya/DFO/grc100_model_results/'
+	kit_storage6 = '/mnt/storage6/tahya/DFO/kit500_model_results/'
 
 	#get list of paths to the output "sub"directories
 	get_subdirs = lambda output_dir : sorted([output_dir + subdir for subdir in os.listdir(output_dir) if subdir.endswith('000')])
 	grc_subdirs = get_subdirs(grc_storage3) + get_subdirs(grc_storage5)
+	kit_subdirs = get_subdirs(kit_storage6)
 	
 	#get list of paths to the files within all the output "sub"directories
 	def get_subdir_files(subdir_paths):
@@ -23,17 +25,21 @@ def filepaths():
 		for subdir_path in subdir_paths:
 			filepaths = filepaths + [subdir_path + '/' + f for f in os.listdir(subdir_path)]
 		return sorted(filepaths)
-	grc_filepaths = get_subdir_files(grc_subdirs)
+	
+	#getting the list of paths using get_subdir_files
+	if model=="grc100": filepaths = get_subdir_files(grc_subdirs)
+	elif model=="kit500": filepaths = get_subdir_files(kit_subdirs)
 
-	#file suffixes
+	#file suffixes (at least, the ones we want)
 	suffixes = ['1d_grid_T.nc','1h_grid_T.nc','1h_grid_V.nc','1ts_geo_U.nc','1d_grid_U.nc','1h_grid_U_2D.nc','1ts_geo_T_2D.nc','1ts_geo_V_2D.nc','1d_grid_V.nc','1h_grid_U.nc','1ts_geo_T.nc','1ts_geo_V.nc','1h_grid_T_2D.nc','1h_grid_V_2D.nc','1ts_geo_U_2D.nc','1ts_geo_W.nc']
-
+	
 	#saving the filepath lists
 	for suffix in suffixes:
-		filepaths_to_save = [filepath for filepath in grc_filepaths if filepath.endswith(suffix)]
-		with open('filepaths/grc_filepaths_' + suffix[:-3] + '.csv', 'w', newline='') as output:
+		filepaths_to_save = [filepath for filepath in filepaths if filepath.endswith(suffix)]
+		with open('filepaths/'+model+'_filepaths_' + suffix[:-3] + '.csv', 'w', newline='') as output:
 			write = csv.writer(output, quoting=csv.QUOTE_ALL)
 			write.writerow(filepaths_to_save)
 
 if __name__ == '__main__':
-	filepaths()
+	for model in ['kit500','grc100']:
+		filepaths(model)
